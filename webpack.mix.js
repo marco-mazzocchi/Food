9dd9bf32f2858107e041e9635cab7b598b7d1c8a
Marco Mazzocchi <marco.mazzocchi.pc@gmail.com>
2018-11-05 19:49:20 +0100

    manage service worker manually (without plugin)

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,36 +14,36 @@ let mix = require('laravel-mix');
 mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-mix.webpackConfig({
-   plugins: [
-       new SWPrecacheWebpackPlugin({
-           cacheId: 'pwa',
-           filename: 'service-worker.js',
-           staticFileGlobs: [
-               'public/**/*.{css,eot,png,svg,ttf,woff,woff2,js,html}'
-           ],
-           minify: true,
-           stripPrefix: 'public/',
-           handleFetch: true,
-           dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
-               //and have full support for offline first (example below)
-               '/': ['resources/views/welcome.blade.php'],
-               '/api/dishes': ['app/Http/Resources/Dish.php']
-           },
-           staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/]
-           // navigateFallback: '/',
-           // runtimeCaching: [
-           //     {
-           //         urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
-           //         handler: 'cacheFirst'
-           //     },
-           //     {
-           //         urlPattern: /^https:\/\/use\.fontawesome\.com\//,
-           //         handler: 'cacheFirst'
-           //     }
-           // ],
-           // importScripts: ['./js/push_message.js']
-       })
-   ]
-});
+// var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+// mix.webpackConfig({
+//    plugins: [
+//        new SWPrecacheWebpackPlugin({
+//            cacheId: 'pwa',
+//            filename: 'service-worker.js',
+//            staticFileGlobs: [
+//                'public/**/*.{css,eot,png,svg,ttf,woff,woff2,js,html}'
+//            ],
+//            minify: true,
+//            stripPrefix: 'public/',
+//            handleFetch: true,
+//            dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+//                //and have full support for offline first (example below)
+//                '/': ['resources/views/welcome.blade.php'],
+//                '/api/dishes': ['app/Http/Resources/Dish.php']
+//            },
+//            staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/]
+//            navigateFallback: '/',
+//            runtimeCaching: [
+//                {
+//                    urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+//                    handler: 'cacheFirst'
+//                },
+//                {
+//                    urlPattern: /^https:\/\/use\.fontawesome\.com\//,
+//                    handler: 'cacheFirst'
+//                }
+//            ],
+//            importScripts: ['./js/push_message.js']
+//        })
+//    ]
+// });
